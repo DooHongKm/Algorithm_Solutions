8e000d6d078cfe49fb71ac68acc6686fd5463a21
DooHongKm <127850414+DooHongKm@users.noreply.github.com>
2023-09-10 08:22:33 +0900

    [Silver II] Title: 마인크래프트, Time: 368 ms, Memory: 117400 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -3,9 +3,16 @@ import sys
 n, m, b = map(int, sys.stdin.readline().split())
 shape = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 
+sum_ = 0
+min_ = 256
+for s in shape:
+  sum_ += sum(s)
+  min_ = min(min_, min(s))
+max_ = (sum_ + b) // (n * m)
+
 best_floor = 0
 best_time = sys.maxsize
-for i in range(257):
+for i in range(min_, max_+1):
   temp1 = 0
   temp2 = 0
   for row in shape:
