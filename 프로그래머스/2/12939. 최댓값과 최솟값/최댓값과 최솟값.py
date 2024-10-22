d48a2aedb00f1cb5a9d0ada49d65a8b2b004b103
DooHongKm <127850414+DooHongKm@users.noreply.github.com>
2024-10-22 22:30:34 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -3,6 +3,6 @@ def solution(s):
     numbers = list(map(int, s.split()))
     max_ = max(numbers)
     min_ = min(numbers)
-    answer = f"{min_} {max_}"
+        
+    return f"{min_} {max_}"
     
-    return answer
