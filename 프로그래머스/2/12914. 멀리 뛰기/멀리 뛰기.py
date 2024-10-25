07814408d45547ffe414b5f4311e4555e644f57b
DooHongKm <127850414+DooHongKm@users.noreply.github.com>
2024-10-25 15:30:25 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.26 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,16 +1,16 @@
+# count(n) = count(n-1) + count(n-2)
+
 def solution(n):
     
-    if n == 1:
-        return 1
-    elif n == 2 :
-        return 2
-    
-    stack = [0 for _ in range(n + 1)]
-    stack[1] = 1
-    stack[2] = 2
-    for i in range(3, n + 1):
-        stack[i] = stack[i - 2] + stack[i - 1]
+    temp1 = 1
+    temp2 = 2
+    for i in range(3, n+1):
+        temp3 = temp1 + temp2
+        temp1 = temp2
+        temp2 = temp3
         
-    answer = stack[n] % 1234567  
+    if n < 3: return n
+    return temp2 % 1234567
+        
+
     
-    return answer
