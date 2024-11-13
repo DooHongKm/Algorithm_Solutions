e81d8f0082b7b195c86ed320c9f057bf061404e7
DooHongKm <127850414+DooHongKm@users.noreply.github.com>
2024-11-13 14:12:42 +0900

    [level 2] Title: H-Index, Time: 299.22 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,18 +1,10 @@
 def solution(citations):
 
-    a = len(citations)
-    b = max(citations)
-    i = min(a, b)
-    
-    while True:
-        count = 0
-        for citation in citations:
-            if citation >= i:
-                count += 1
-        if count >= i:
-            break
-        else:
-            i -= 1
-            continue
-            
-    return i
+    max_ = max(citations)
+    for i in range(max_, -1, -1):
+        big = 0
+        for c in citations:
+            if c >= i:
+                big += 1
+        if big >= i:
+            return i
