eb2463a8dc86426865a410a22415d7bc6578702b
DooHongKm <127850414+DooHongKm@users.noreply.github.com>
2024-11-17 07:43:24 +0900

    [D4] Title: 격자판의 숫자 이어 붙이기, Time: 324 ms, Memory: 71,560 KB -BaekjoonHub

--- SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.py
+++ SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.py
@@ -1,29 +1,26 @@
 T = int(input())
 for count in range(1, T + 1):
 
-    board = [list(map(int, input().split())) for _ in range(4)]
-
     dy = [0, 0, -1, 1]
     dx = [-1, 1, 0, 0]
+    
+    board = [input().split() for _ in range(4)]
 
     numbers = []
 
     def dfs(x, y, t):
         if len(t) == 7:
-            if t not in numbers:
-                numbers.append(t)
+            numbers.append(t)
             return
-        if x > 0:
-            dfs(x - 1, y, t + str(board[y][x - 1]))
-        if x < 3:
-            dfs(x + 1, y, t + str(board[y][x + 1]))
-        if y > 0:
-            dfs(x, y - 1, t + str(board[y - 1][x]))
-        if y < 3:
-            dfs(x, y + 1, t + str(board[y + 1][x]))
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < 4 and 0 <= ny < 4:
+                dfs(nx, ny, t + board[ny][nx])
 
     for i in range(4):
         for j in range(4):
-            dfs(j, i, str(board[i][j]))
+            dfs(j, i, board[i][j])	
+    numbers = set(numbers)
 
     print(f"#{count} {len(numbers)}")
