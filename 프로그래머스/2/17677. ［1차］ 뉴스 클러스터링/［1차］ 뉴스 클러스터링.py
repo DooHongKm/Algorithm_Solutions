bd5614feaa4353cac2ba0993885d15a16fe83da7
DooHongKm <127850414+DooHongKm@users.noreply.github.com>
2024-11-20 10:39:39 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.63 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -3,21 +3,30 @@ def solution(str1, str2):
     str1 = str1.lower()
     str2 = str2.lower()
     
-    a = [str1[i:i+2] for i in range(len(str1)-1) if str1[i].isalpha() and str1[i+1].isalpha()]
-    b = [str2[i:i+2] for i in range(len(str2)-1) if str2[i].isalpha() and str2[i+1].isalpha()]
-    la = len(a)
-    lb = len(b)
-
-    gyo = 0
-    for i in a:
-        if i in b:
-            b.remove(i)
-            gyo += 1
-    hap = la + lb - gyo
+    temp1 = [str1[i:i+2] for i in range(len(str1) - 1) if str1[i].isalpha() and str1[i+1].isalpha()]
+    temp2 = [str2[i:i+2] for i in range(len(str2) - 1) if str2[i].isalpha() and str2[i+1].isalpha()]
     
+    dict1 = {}
+    dict2 = {}
+    for t in temp1:
+        if t in dict1:
+            dict1[t] += 1
+        else:
+            dict1[t] = 1
+    for t in temp2:
+        if t in dict2:
+            dict2[t] += 1
+        else:
+            dict2[t] = 1
+            
+    kyo = 0
+    for key in dict1:
+        if key in dict2:
+            kyo += min(dict1[key], dict2[key])
+            
+    hap = len(temp1) + len(temp2) - kyo
     if hap == 0:
-        result = 65536
+        return 65536
     else:
-        result = int(gyo / hap * 65536)
+        return int(kyo / hap * 65536)
     
-    return result
