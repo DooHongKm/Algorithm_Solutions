c6669cfa82ce0ae2be21ade3ac80f36868b18693
DooHongKm <127850414+DooHongKm@users.noreply.github.com>
2024-11-21 10:21:32 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.69 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 def solution(str1, str2):
 
     str1 = str1.lower()
@@ -6,27 +8,16 @@ def solution(str1, str2):
     temp1 = [str1[i:i+2] for i in range(len(str1) - 1) if str1[i].isalpha() and str1[i+1].isalpha()]
     temp2 = [str2[i:i+2] for i in range(len(str2) - 1) if str2[i].isalpha() and str2[i+1].isalpha()]
     
-    dict1 = {}
-    dict2 = {}
-    for t in temp1:
-        if t in dict1:
-            dict1[t] += 1
-        else:
-            dict1[t] = 1
-    for t in temp2:
-        if t in dict2:
-            dict2[t] += 1
-        else:
-            dict2[t] = 1
-            
-    kyo = 0
-    for key in dict1:
-        if key in dict2:
-            kyo += min(dict1[key], dict2[key])
-            
-    hap = len(temp1) + len(temp2) - kyo
+    counter1 = Counter(temp1)
+    counter2 = Counter(temp2)
+    
+    gyo = sum((counter1 & counter2).values())
+    hap = sum((counter1 | counter2).values())
     if hap == 0:
         return 65536
     else:
-        return int(kyo / hap * 65536)
+        return int(gyo / hap * 65536)
+    
+    
+    
     
