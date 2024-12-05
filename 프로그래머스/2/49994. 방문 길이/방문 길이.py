f11f10fc6c6468f4bd8e283c91dfd8787e439680
DooHongKm <127850414+DooHongKm@users.noreply.github.com>
2024-12-05 14:40:55 +0900

    [level 2] Title: 방문 길이, Time: 0.70 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,27 +1,24 @@
 def solution(dirs):
     
-    stack = [(0, 0)]
+    direction = {"U":(0, 1), "D":(0, -1), "L":(-1, 0), "R":(1, 0)}
     
-    dir_dict = {}
-    dir_dict['U'] = (0, 1)
-    dir_dict['D'] = (0, -1)
-    dir_dict['R'] = (1, 0)
-    dir_dict['L'] = (-1, 0)
-    
-    x = 0
-    y = 0
+    visited = [(0, 0)]
     for d in dirs:
-        x = max(min(x + dir_dict[d][0], 5), -5)
-        y = max(min(y + dir_dict[d][1], 5), -5)
-        if (x, y) != stack[-1]:
-            stack.append((x, y))
-            
-    paths = set()
-    for i in range(len(stack)-1):
-        paths.add(stack[i] + stack[i + 1])
-        paths.add(stack[i + 1] + stack[i])
-            
-    answer = len(paths) // 2
-    
-    return answer
+        dx, dy = direction[d]
+        cx, cy = visited[-1]
+        nx = cx + dx
+        ny = cy + dy
+        if nx < -5 or nx > 5 or ny < -5 or ny > 5:
+            continue
+        visited.append((nx, ny))
+        
+    result = set()
+    for i in range(len(visited) - 1):
+        x, y = visited[i]
+        nx, ny = visited[i + 1]
+        result.add((x, y, nx, ny))
+        result.add((nx, ny, x, y))
+        
+    return len(result) // 2
+        
     
